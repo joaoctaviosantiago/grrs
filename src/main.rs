8fd3399919afd685b8e85d3c78557d9ebb66dd38
João Octávio Santiago <jomsoutosantiago@gmail.com>
2024-08-01 03:02:54 -0300

    Minimum Viable Product; Includes "--help" flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let file = File::open(&args.path).expect("Could not read file.");
+    let reader = BufReader::new(file).lines();
+
+    for line in reader.map_while(Result::ok) {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
